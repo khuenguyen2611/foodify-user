ab58631862ef7ec3650dbd584d2daaf3f1888dba
Nguyen Ha Khue <khuecuxi3@gmail.com>
2023-04-29 22:20:07 +0700

    Change default img

--- src/app/shared/classes/register.ts
+++ src/app/shared/classes/register.ts
@@ -7,7 +7,7 @@ export class Register {
     identifiedCode: string;
     email: string;
     phoneNumber: string;
-    imageUrl = 'https://firebasestorage.googleapis.com/v0/b/foodify-55954.appspot.com/o/Admin%2Fuser-default-01.png?alt=media&token=b13f27da-b39a-4d89-9cba-acba869d60ce';
+    imageUrl = 'https://firebasestorage.googleapis.com/v0/b/foodify-55954.appspot.com/o/Admin%2Fdefault%2Fdefault-ava-01.png?alt=media&token=7175d053-86b9-4bee-8efa-221c2e2b8da9';
     isLocked = false;
     roleName = 'ROLE_USER';
     addressDto: Address;
