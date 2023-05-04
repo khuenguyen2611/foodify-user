8b74d648b9402ebe3990db766fcdeae595a89e71
Nguyen Ha Khue <khuecuxi3@gmail.com>
2023-05-04 20:10:52 +0700

    Deployment 5.4.2023 v1.1
    Fix UI

--- src/app/pages/account/order/checkout.component.ts
+++ src/app/pages/account/order/checkout.component.ts
@@ -12,7 +12,7 @@ import { UserService } from 'src/app/shared/services/user.service';
     templateUrl: './checkout.component.html',
     styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, OnDestroy {
     private userId: number;
     private token: string = localStorage.getItem('jwt-token')
 
@@ -26,8 +26,7 @@ export class CheckoutComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private userService: UserService,
-        private orderService: OrderService,
-        private firebaseService: FirebaseService
+        private orderService: OrderService
     ) {
 
     }
@@ -39,6 +38,7 @@ export class CheckoutComponent implements OnInit {
             this.userId = userInfo.userId;
             this.orderService.getOrderById(this.userId, id).subscribe((order) => {
                 this.order = order;
+                console.log(order.status)
                 if (this.order.status == 'AWAITING') {
                     this.currentStep = 1;
                 }
@@ -71,5 +71,45 @@ export class CheckoutComponent implements OnInit {
         if (this.currentStep > this.numSteps + 1) {
             this.currentStep = 1;
         }
+        var stepper = document.getElementById("stepper1");
+        var steps = stepper.getElementsByClassName("step");
+
+        Array.from(steps).forEach((step, index) => {
+            let stepNum = index + 1;
+            if (stepNum === this.currentStep) {
+                this.addClass(step, "editing");
+            } else {
+                this.removeClass(step, "editing");
+            }
+            if (stepNum < this.currentStep) {
+                this.addClass(step, "done");
+            } else {
+                this.removeClass(step, "done");
+            }
+        });
+    }
+
+    hasClass(elem, className) {
+        return new RegExp(" " + className + " ").test(" " + elem.className + " ");
+    }
+
+    addClass(elem, className) {
+        if (!this.hasClass(elem, className)) {
+            elem.className += " " + className;
+        }
+    }
+
+    removeClass(elem, className) {
+        var newClass = " " + elem.className.replace(/[\t\r\n]/g, " ") + " ";
+        if (this.hasClass(elem, className)) {
+            while (newClass.indexOf(" " + className + " ") >= 0) {
+                newClass = newClass.replace(" " + className + " ", " ");
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, "");
+        }
+    }
+
+    ngOnDestroy(): void {
+        clearTimeout(this.refreshTimeout);
     }
 }
