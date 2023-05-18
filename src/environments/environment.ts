345d3ecc62ef088d24aef50cac9857799af5039a
Nezumi <123229563+Nez27@users.noreply.github.com>
2023-05-18 19:47:00 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   stripe_token: 'pk_test_51Kr70iBMfPdj9f7ncBKwqgrPwiLIyDrvlFchwOTcXfomHjnC7i0coMPmwGXnuBhenxVElvLF2ojiP9fda3KYIohP00usHs6HxM',
   paypal_token: 'PAYPAL_TOKEN',
   // foodOrderingBaseApiUrl: 'http://localhost:8080/api',
-  foodOrderingBaseApiUrl: 'https://foodify-backend-production.up.railway.app/api',
+  foodOrderingBaseApiUrl: 'https://foodify.up.railway.app/api',
   firebaseConfig: {
     apiKey: 'AIzaSyAZFFIuXkbgdp2F-Em4CK2z8kVJ2L4p_UU',
     authDomain: 'foodify-55954.firebaseapp.com',
